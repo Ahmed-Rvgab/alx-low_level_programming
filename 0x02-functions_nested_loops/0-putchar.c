899d2369bd37ffde0affeb205f4ed422a1fe94ce
rvgab <ragabb510@gmail.com>
2023-03-21 15:36:18 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+** main-Print_putchar
+**
+** Return:Always0
+**/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
